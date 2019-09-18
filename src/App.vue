abbccb79747b2794a18e773c8c529b40ae5d800c
minixz <minikuz@gmail.com>
2019-09-18 06:25:30 -0700

    Basic setup

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h1>DreamCrafter Lottery Table</h1>
+    <BlockItem
+      src="images/block/yellow_glazed_terracotta.png"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import BlockItem from './components/BlockItem.vue';
+
+@Component({
+  components: {
+    BlockItem,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
