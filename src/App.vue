f4313cce353553438b427924de4cb09123843277
minixz <minikuz@gmail.com>
2019-09-21 22:55:11 -0700

    Update styles

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <template>
   <div id="app">
-    <!-- <h1>DreamCrafter Lottery Table</h1> -->
+    <h1 class="app__title">
+      <div
+        class="app__container"
+      >
+        DreamCrafter <span class="title__lottery">抽抽樂</span> 一覧
+      </div>
+    </h1>
     <div
-      class="app__lottery-tables"
+      class="app__container"
     >
-      <LotteryTable
-        v-for="(lottery, id) in config.lotteries"
-        :key="id"
-        :value="lottery"
-        :config-items="config.items"
-        class="app__lottery-table"
-      />
+      <div
+        class="app__lottery-tables"
+      >
+        <LotteryTable
+          v-for="(lottery, id) in config.lotteries"
+          :key="id"
+          :value="lottery"
+          :config-items="config.items"
+          class="app__lottery-table"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -47,23 +57,37 @@ export default class App extends Vue {
 
 body {
   font-family: unifont, Arial, sans-serif;
-  background-color: #202020;
+  background-color: #303030;
+  color: #ffffff;
 }
 
-#app {
+.app__title {
+  font-size: 30px;
+  margin: 0 0 10px 0;
+  padding: 20px 0;
+  background-color: #000;
+}
+
+.app__container {
   margin: 0 auto;
   max-width: 1200px;
-  color: #ffffff;
-
   @media screen and (max-width: 1200px) {
     margin: 0 10px;
   }
+}
+
+.title__lottery {
+  background-color: #ffff00;
+  color: #ff0000;
+  padding: 3px 2px;
+  font-weight: bold;
 }
 
 .app__lottery-tables {
   display: flex;
   flex-wrap: wrap;
   justify-content: space-between;
+  margin: 0 -10px;
 
   @media screen and (max-width: 780px) {
     justify-content: center;
